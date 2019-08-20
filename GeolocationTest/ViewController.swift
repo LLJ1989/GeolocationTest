b0b03d336d2201f098422f642cdd5d8f0d97bf3c
Lucas <l.lucaslombard@free.fr>
2019-08-20 19:51:56 +0200

    Initial Commit

---
+++ GeolocationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeolocationTest
+//
+//  Created by Lucas Lombard on 20/08/2019.
+//  Copyright © 2019 Lucas Lombard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
